6ee4f0eb550890659955413a13810ee8571439fd
FerCappelletti <fer_cappelletti@hotmail.com>
2023-06-21 14:39:17 -0300

    server configured, up && running locally

--- login/lib/api.js
+++ login/lib/api.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+// Routes
+const router = (0, express_1.Router)();
+//router.use("/users", userRoutes);
+exports.default = router;
--- login/lib/index.js
+++ login/lib/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('Server');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = require("./utils/cors");
+const config_1 = require("./config/config");
+const app = (0, express_1.default)();
+const port = config_1.config.PORT || 3000;
+app.use(express_1.default.json());
+/** Ping server for health purpose */
+app.get('/healthCheck', (req, res, next) => res.status(200).json({ message: 'Hey...you ping me!' }));
+app.use((req, res, next) => {
+    (0, cors_1.corsEnabledFunction)(req, res);
+    next();
+});
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+}).on('error', function (err) {
+    console.log(err);
+    process.exit(1);
+});
+module.exports = app;
