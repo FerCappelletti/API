261d813e3af8edb61f0e9ba4521d93a7ea1ca9c3
FerCappelletti <fer_cappelletti@hotmail.com>
2023-06-22 17:43:56 -0300

    fix redirect endpoint

--- login/lib/entities/user/user.routes.js
+++ login/lib/entities/user/user.routes.js
@@ -12,6 +12,10 @@ router.get('/', (req, res) => {
 router.post('/login', user_controller_1.login);
 router.post('/register', user_controller_1.register);
 router.get('/all', (req, res) => {
-    res.redirect('http://localhost:3000/api/v1/auth/users');
+    // const user = JSON.stringify( {
+    //     email: req.body.email,
+    //     token: req.body.token
+    // })
+    res.redirect(`http://localhost:3000/api/v1/auth/users/${req.body.email}/${req.body.token}`);
 });
 exports.default = router;
